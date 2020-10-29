4e4666ad0972fe102bd87674be5e913e906a0b14
Dipin Garg <dgargdipin@gmail.com>
2020-10-29 22:24:42 +0530

    adding courses now working

--- cms/professor/forms.py
+++ cms/professor/forms.py
@@ -5,7 +5,7 @@ from wtforms import ValidationError
 from flask_wtf.file import FileField,FileAllowed
 
 from flask_login import current_user
-from puppycompanyblog.models import User
+from cms.models import Professor,Course
 
 class LoginForm(FlaskForm):
     email=StringField('Email',validators=[DataRequired('Data required'),Email('email required')])
@@ -13,24 +13,31 @@ class LoginForm(FlaskForm):
     submit=SubmitField('Login')
 
 class RegistrationForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired('Data required')])
     email=StringField('Email',validators=[DataRequired('Data required'),Email('email required')])
     password=PasswordField('Password',validators=[DataRequired('Data required'),
     EqualTo('pass_confirm',message="Passwords must match!")])
     pass_confirm=PasswordField('Confirm Password',validators=[DataRequired('Data required')])
-    year = SelectField('Year of study', choices=[
-                       (1, '1st'), (2, '2nd'), (3, '3rd'),(4,'4th')])
     branch = SelectField('Branch', validators=[DataRequired('Data required')],
                                             choices=[('CSE',"Computer Science"),('EE','Electrical'),('ME','Mechanical'),('CE','Civil')])
-    submit=SubmitField('Register')
+    submit=SubmitField('Regiss  ster')
     def validate_email(self,field):
-        if User.query.filter_by(email=field.data).first():
+        if Professor.query.filter_by(email=field.data).first():
             raise ValidationError('Your email has been registered already')
     
 
-class UpdateUserForm(FlaskForm):
+class UpdateProfForm(FlaskForm):
     email=StringField('Email',validators=[DataRequired('Data required'),Email('email required')])
     submit=SubmitField('Update')
     def check_email(self,field):
-        if User.query.filter_by(email=field.data).first():
+        if Professor.query.filter_by(email=field.data).first():
             raise ValidationError('Your email has been registered already')
     
+class addCourseForm(FlaskForm):
+    name = StringField('Course Title', validators=[DataRequired('Data required')])
+    course_code = StringField('Course code', validators=[DataRequired('Data required')])
+    details = StringField('Course Details', validators=[DataRequired('Data required')])
+    submit = SubmitField('Add Course')
+    def validate_course_code(self,field):
+        if Course.query.filter_by(course_code=field.data).first():
+            raise ValidationError('Course code has already been registered')
